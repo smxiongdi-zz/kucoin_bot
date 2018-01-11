6bd5dd36c9e1c1b0ad644552024471d5fdd2ad08
ksbkek <ksbkek@gmail.com>
2018-01-11 02:26:40 +0000

    cleanup

--- api/cmc.js
+++
@@ -1,8 +0,0 @@
-let fetch = require('node-fetch');
-
-const CoinMarketCapAPI = () => {
-    const baseUrl = "https://api.coinmarketcap.com/v1/ticker/";
-    return fetch(baseUrl).then(res => res.json());
-}
-
-module.exports = CoinMarketCapAPI;
--- db/connect/eth_coins_connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/eth_coins");
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "Connection Error: "));
-db.once("open", function() {});
-
-module.exports = db;
